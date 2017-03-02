50fb4f37d27c5162eedd4bdc402281043a36a2ac
kimCyton <2143039855@qq.com>
2017-03-02 19:45:15 +0800

    'todolist_Demo'

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,75 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
+    <h2>TodoList</h2>
+    <input type="text" v-model='content' v-on:keyup.enter="addNew">
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for='item in todos' v-bind:class="{completed:item.completed}" v-on:click="toggleComp(item)">
+        <span>{{item.task}}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span @click="remove(item.id)">X</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Store from './store'
 export default {
   name: 'app',
-  data () {
+  data:function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todos:Store.fetch(),
+      content:''
+    }
+  },
+  methods:{
+    toggleComp:function(item){
+      item.completed=!item.completed
+    },
+    addNew:function(){
+      console.log(this.content)
+      this.todos.push({
+        task:this.content,
+        id:new Date().getTime(),
+        completed:false
+      });
+      this.content='';
+      //console.log(Storage)
+    },
+    remove:function(id){
+      var _this = this;
+      this.todos.forEach(function(value,index){
+        if (value.id==id) {
+          _this.todos.splice(index,1)
+        };
+      })
+    }
+  },
+  watch:{
+    todos:{
+      handler:function(items){
+        Store.save(items)
+      },
+      deep:true
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+.completed span:nth-of-type(1){
+  text-decoration: line-through;
+}
+
+span:nth-of-type(2){
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  background: #39c;
+  color:#fff;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -47,11 +86,6 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 
 a {

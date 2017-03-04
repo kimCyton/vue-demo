ef1cc49f833c45c1162d35c247b98e221d356d65
kimCyton <2143039855@qq.com>
2017-03-04 08:26:07 +0800

    'import_data'

---
+++ src/components/footer/data.js
@@ -0,0 +1,8 @@
+export default {
+	data_detail:[
+		{title:'消费者保障',detailLinks:[{link:'#',content:'保障范围'},{link:'#',content:'退货退款流程'},{link:'#',content:'服务中心'},{link:'#',content:'更多特色服务'}]},
+		{title:'新手上路',detailLinks:[{link:'#',content:'新手专区'},{link:'#',content:'消费警示'},{link:'#',content:'交易安全'},{link:'#',content:'24小时在线帮助'},{link:'#',content:'免费开店'}]},
+		{title:'淘宝特色',detailLinks:[{link:'#',content:'手机淘宝'},{link:'#',content:'旺信'},{link:'#',content:'大众点评'}]},
+		{title:'付款方式',detailLinks:[{link:'#',content:'快捷支付'},{link:'#',content:'信用卡'},{link:'#',content:'余额宝'},{link:'#',content:'蚂蚁花呗'},{link:'#',content:'货到付款'}]}
+	]
+}
--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<footer class="footer">
-		<div v-for="item in customService" class='f_p f_part1'>
+		<div v-for="item in customService" class='f_p'>
 			<h4 class="title">{{item.title}}</h4>
 			<p class="detailLinks">
 				<a v-for="obj in item.detailLinks" :href="obj.link">{{obj.content}}</a>
@@ -10,17 +10,13 @@
 </template>
 
 <script>
+import content from './data.js'
 export default {
   name: 'footer',
 
   	data () {
     	return {
-    		customService:[
-    			{title:'消费者保障',detailLinks:[{link:'#',content:'保障范围'},{link:'#',content:'退货退款流程'},{link:'#',content:'服务中心'},{link:'#',content:'更多特色服务'}]},
-    			{title:'新手上路',detailLinks:[{link:'#',content:'新手专区'},{link:'#',content:'消费警示'},{link:'#',content:'交易安全'},{link:'#',content:'24小时在线帮助'},{link:'#',content:'免费开店'}]},
-    			{title:'淘宝特色',detailLinks:[{link:'#',content:'手机淘宝'},{link:'#',content:'旺信'},{link:'#',content:'大众点评'}]},
-    			{title:'付款方式',detailLinks:[{link:'#',content:'快捷支付'},{link:'#',content:'信用卡'},{link:'#',content:'余额宝'},{link:'#',content:'蚂蚁花呗'},{link:'#',content:'货到付款'}]}
-    		]
+    		customService:content.data_detail
     	};
   	}
 };
@@ -31,7 +27,7 @@ export default {
 		width:980px;
 		margin:0 auto;
 		background:#f1f1f1;
-		margin-top: 730px;
+		margin-top: 30px;
 		.f_p{
 			width: 25%;
 			float: left;

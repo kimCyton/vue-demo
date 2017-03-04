d9401cabbcfb38b3482cbe940023bb8d7c1e2979
kimCyton <2143039855@qq.com>
2017-03-04 10:26:42 +0800

    'use_of_animate_in_vuejs'

--- src/components/transition/transition.vue
+++ src/components/transition/transition.vue
@@ -1,9 +1,13 @@
 <template>
 	<div id="demo">
-		<button class="btn btn-default" v-on:click="show = !show">
+		<button class="btn btn-default btn-sm" v-on:click="show = !show">
 			Toggle
 		</button>
-		<transition name="fade">
+		<transition
+		    name="custom-classes-transition"
+		    enter-active-class="animated tada"
+		    leave-active-class="animated bounceOutRight"
+		  >
 			<p v-if="show">hello</p>
 		</transition>
 	</div>
@@ -20,10 +24,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s
-}
-.fade-enter, .fade-leave-active {
-  opacity: 0
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './css/index.scss'
 import './../node_modules/bootstrap/dist/css/bootstrap.css'
+import './../node_modules/animate.css'
 
 new Vue({
   el: '#app',
